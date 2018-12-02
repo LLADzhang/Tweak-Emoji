4d28b172fe5facb892e2f18c5a4ff36445e869c2
Heng <LLADzhang>
2018-12-02 16:55:55 -0500

    split train and test data

--- code_version1/organize_data.py
+++ code_version1/organize_data.py
@@ -16,6 +16,7 @@ if __name__ == '__main__':
 
     outDir = args.output
     trainDir = os.path.join(outDir, 'train')
+    testDir = os.path.join(outDir, 'test')
 
     for section in os.listdir(labelsDir):
         if section[0] != '.':
@@ -30,8 +31,18 @@ if __name__ == '__main__':
                             label = line.split('.')[0].strip()
                             if classes[int(label)] not in os.listdir(trainDir):
                                 os.mkdir(os.path.join(trainDir, classes[int(label)]))
+                            if classes[int(label)] not in os.listdir(testDir):
+                                os.mkdir(os.path.join(testDir, classes[int(label)]))
 
                             imgName = labelFile[0:-12] + '.png'
                             imgPath = os.path.join(imgsDir, section, seq, imgName)
                             savePath = os.path.join(trainDir, classes[int(label)], imgName)
                             shutil.copyfile(imgPath, savePath)
+
+    for label in os.listdir(trainDir):
+        n = len(os.listdir(os.path.join(trainDir, label)))
+        nTest = int(n * 0.2)
+        
+        for image in os.listdir(os.path.join(trainDir, label))[: nTest + 1]:
+            shutil.move(os.path.join(trainDir, label, image), os.path.join(testDir, label, image))
+
